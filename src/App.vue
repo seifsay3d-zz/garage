682375359817e13d08a9fbf943ef9ca86afd0a2a
seifsayed <seif.sayed@gmail.com>
2017-06-16 20:00:35 +0200

    project setup

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header></header>
+    <page-wrapper></page-wrapper>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+import GHeader from './Layout/Header';
+import GFooter from './Layout/Footer';
+import PageWrapper from './Layout/PageWrapper';
+
+export default {
+  name: 'app',
+  components: {
+    GHeader,
+    GFooter,
+    PageWrapper,
+  },
+};
+</script>
+
+<style lang="scss">
+  @import '~bulma';
+  
+</style>
