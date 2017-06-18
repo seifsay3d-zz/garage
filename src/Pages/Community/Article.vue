8d2016d2f204fef38d1086f2e14cf0433ff2db27
seifsayed <seif.sayed@gmail.com>
2017-06-18 22:38:21 +0200

    update pages to use components

--- src/Pages/Community/Article.vue
+++ src/Pages/Community/Article.vue
@@ -1,80 +1,13 @@
 <template>
-<div>
-  <section class="hero is-primary is-medium">
-    <div class="hero-body">
-      <div class="container has-text-centered">
-        <h1 class="title is-2">
-          Blog post title
-        </h1>
-        <h2 class="subtitle is-4">
-          Blog post subtitle
-        </h2>
-      </div>
-    </div>
-  </section>
-  <section class="section blog">
-    <div class="container">
-      <div class="columns is-mobile">
-        <div class="column is-8 is-offset-2">
-          <div class="content blog-post section">
-            <p class="has-text-right has-text-muted">published 2 days ago</p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit massa eget mauris hendrerit, sit amet laoreet ex mattis. Aenean finibus massa eget finibus faucibus. Etiam dolor lacus, imperdiet et nisl vitae, lacinia euismod arcu. Aliquam non sapien hendrerit nisi semper rhoncus a eget erat. Maecenas accumsan semper ante eu molestie. Integer tristique erat sit amet posuere cursus. Aliquam placerat sit amet lectus laoreet vestibulum. Sed pharetra neque ac libero ornare, nec viverra ante tristique. Mauris eget urna metus.
-            </p>
-
-            <p>
-              Nunc blandit leo orci, ornare consequat lacus cursus ac. In tincidunt nulla pharetra, vulputate nisl non, molestie tellus. Pellentesque consectetur dolor id diam mattis, at luctus nibh ultricies. Sed ac mi a est venenatis accumsan nec sed urna. Nullam ut interdum elit. Nulla facilisi. Suspendisse lacinia nisi in tincidunt ornare. Nam lobortis justo eu sapien vestibulum, et ullamcorper orci iaculis. Nullam at nulla in eros dictum condimentum vel id ante.
-            </p>
-
-            <p>
-              Morbi mollis ipsum vitae turpis molestie, sollicitudin dictum nunc condimentum. Nullam lobortis metus vitae ligula dictum, in congue est egestas. Aliquam enim massa, fermentum sed massa ut, varius sagittis metus. In eu nisi at eros tincidunt feugiat vitae sed libero. Aliquam urna metus, venenatis id feugiat tristique, sagittis in nunc. Sed volutpat eget neque sit amet convallis. Sed maximus quam sed magna congue, in gravida leo commodo. Mauris auctor nulla ultricies aliquam pharetra. Nulla in tristique sem, nec pellentesque erat. Donec a porttitor leo, vel sollicitudin lectus. Donec dictum mauris non congue efficitur.
-            </p>
-
-            <p>
-              Morbi auctor consequat facilisis. Fusce eu vehicula tellus, eget imperdiet lacus. Praesent dapibus rutrum tellus, vitae sollicitudin nulla tincidunt a. In metus felis, aliquam aliquet convallis eu, vulputate vel nunc. Etiam metus odio, tristique in tristique sed, congue ultricies sapien. Vestibulum velit massa, rhoncus vel suscipit sed, mattis nec odio. Cras justo lectus, tincidunt in interdum vitae, finibus nec neque. Nullam dolor purus, bibendum ut ex vitae, tristique fringilla quam. Sed dapibus mi erat, ut efficitur nulla vulputate id. Quisque turpis ligula, fermentum id sagittis vitae, fermentum vel magna. Nunc neque nunc, commodo nec facilisis eget, mollis mollis massa. Quisque venenatis maximus turpis vitae rhoncus.
-            </p>
-
-            <p>
-              Aenean at dignissim lacus. Etiam id dolor mauris. Aenean mollis sollicitudin blandit. Nulla elementum, metus ut commodo gravida, diam ligula volutpat mauris, dignissim lacinia eros ipsum sit amet dolor. Ut vestibulum sem nec luctus interdum. Integer ut efficitur nisl. Nam eu neque turpis.
-            </p>
-          </div>
-          <div class="card is-fullwidth">
-            <header class="card-header">
-              <p class="card-header-title">
-                About the author
-              </p>
-              <a class="card-header-icon" href="#top">
-                <i class="fa fa-angle-up"></i>
-              </a>
-            </header>
-            <div class="card-content">
-              <article class="media">
-                <div class="media-left">
-                  <figure class="image is-64x64">
-                    <img src="http://placehold.it/128x128" alt="Image">
-                  </figure>
-                </div>
-                <div class="media-content">
-                  <div class="content">
-                    <p>
-                      <strong>John Smith</strong> <small>@johnsmith</small>
-                      <br>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                    </p>
-                  </div>
-                </div>
-              </article>
-            </div>
-            <footer class="card-footer">
-              <a class="card-footer-item">Share on Facebook</a>
-              <a class="card-footer-item">Share on Twitter</a>
-              <a class="card-footer-item">Share on G+</a>
-            </footer>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  </div>
+  <g-article></g-article>
 </template>
 
+<script>
+  import GArticle from '@/Community/Article';
+
+  export default{
+    components: {
+      GArticle,
+    },
+  };
+</script>
--- src/Pages/Home/Index.vue
+++ src/Pages/Home/Index.vue
@@ -21,96 +21,9 @@
               <h2>Recently Added</h2>
             </div>
             <div class="columns">
-              <div class="column">
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-left">
-                        <figure class="image is-48x48">
-                          <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
-                        </figure>
-                      </div>
-                      <div class="media-content">
-                        <p class="title is-4">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-                      </div>
-                    </div>
-
-                    <div class="content">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                      <a>#css</a> <a>#responsive</a>
-                      <br>
-                      <small>11:09 PM - 1 Jan 2016</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="column">
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-left">
-                        <figure class="image is-48x48">
-                          <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
-                        </figure>
-                      </div>
-                      <div class="media-content">
-                        <p class="title is-4">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-                      </div>
-                    </div>
-
-                    <div class="content">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                      <a>#css</a> <a>#responsive</a>
-                      <br>
-                      <small>11:09 PM - 1 Jan 2016</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="column">
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Image">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-left">
-                        <figure class="image is-48x48">
-                          <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
-                        </figure>
-                      </div>
-                      <div class="media-content">
-                        <p class="title is-4">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-                      </div>
-                    </div>
-
-                    <div class="content">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                      <a>#css</a> <a>#responsive</a>
-                      <br>
-                      <small>11:09 PM - 1 Jan 2016</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <product-thumb></product-thumb>
+              <product-thumb></product-thumb>
+              <product-thumb></product-thumb>
             </div>
           </div>
           <div class="column">
@@ -154,131 +67,25 @@
               </div>
             </div>
             <div class="columns">
-              <div class="column">
-                <article class="media">
-                  <figure class="media-left">
-                    <p class="image is-64x64">
-                      <img src="http://bulma.io/images/placeholders/128x128.png">
-                    </p>
-                  </figure>
-                  <div class="media-content">
-                    <div class="field">
-                      <p class="control">
-                      <textarea class="textarea" placeholder="Got a question? have an advice? communicate with your fellow riders .."></textarea>
-                      </p>
-                    </div>
-                    <nav class="level">
-                      <div class="level-right">
-                        <div class="level-item">
-                          <a class="button is-info">Submit</a>
-                        </div>
-                      </div>
-                    </nav>
-                  </div>
-                </article>
-              </div>
+                <new-comment></new-comment>
             </div>
-            <div class="columns">
-              <div class="column">
-                <article class="media">
-                  <figure class="media-left">
-                    <p class="image is-64x64">
-                      <img src="http://bulma.io/images/placeholders/128x128.png">
-                    </p>
-                  </figure>
-                  <div class="media-content">
-                    <div class="content">
-                      <p>
-                        <strong>Barbara Middleton</strong>
-                        <br>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
-                        <br>
-                        <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
-                      </p>
-                    </div>
-
-                    <article class="media">
-                      <figure class="media-left">
-                        <p class="image is-48x48">
-                          <img src="http://bulma.io/images/placeholders/96x96.png">
-                        </p>
-                      </figure>
-                      <div class="media-content">
-                        <div class="content">
-                          <p>
-                            <strong>Sean Brown</strong>
-                            <br>
-                            Donec sollicitudin urna eget eros malesuada sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam blandit nisl a nulla sagittis, a lobortis leo feugiat.
-                            <br>
-                            <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
-                          </p>
-                        </div>
-
-                        <article class="media">
-                          Vivamus quis semper metus, non tincidunt dolor. Vivamus in mi eu lorem cursus ullamcorper sit amet nec massa.
-                        </article>
-
-                        <article class="media">
-                          Morbi vitae diam et purus tincidunt porttitor vel vitae augue. Praesent malesuada metus sed pharetra euismod. Cras tellus odio, tincidunt iaculis diam non, porta aliquet tortor.
-                        </article>
-                      </div>
-                    </article>
-
-                    <article class="media">
-                      <figure class="media-left">
-                        <p class="image is-48x48">
-                          <img src="http://bulma.io/images/placeholders/96x96.png">
-                        </p>
-                      </figure>
-                      <div class="media-content">
-                        <div class="content">
-                          <p>
-                            <strong>Kayli Eunice </strong>
-                            <br>
-                            Sed convallis scelerisque mauris, non pulvinar nunc mattis vel. Maecenas varius felis sit amet magna vestibulum euismod malesuada cursus libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus lacinia non nisl id feugiat.
-                            <br>
-                            <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
-                          </p>
-                        </div>
-                      </div>
-                    </article>
-                  </div>
-                </article>
-              </div>
+            <div class="columns is-multiline">
+                <g-comment class="is-12"></g-comment>
+                <g-comment class="is-12"></g-comment>
+                <g-comment class="is-12"></g-comment>
             </div>
           </div>
           <div class="column">
-            <div class="columns">
+            <div class="columns ">
               <div class="column">
                 <h2>Articles</h2>
               </div>
             </div>
-            <div class="columns">
-              <div class="column">
-                <div class="card">
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-left">
-                        <figure class="image is-48x48">
-                          <img src="http://bulma.io/images/placeholders/96x96.png" alt="Image">
-                        </figure>
-                      </div>
-                      <div class="media-content">
-                        <p class="title is-4">Safety Frist</p>
-                        <p class="subtitle is-6">@seifsayed</p>
-                      </div>
-                    </div>
-
-                    <div class="content">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Phasellus nec iaculis mauris <a>read more</a>
-                      <a>#css</a> <a>#responsive</a>
-                      <br>
-                      <small>11:09 PM - 1 Jan 2016</small>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div class="columns is-multiline">
+              <article-thumb class="is-6"></article-thumb>
+              <article-thumb class="is-6"></article-thumb>
+              <article-thumb class="is-6"></article-thumb>
+              <article-thumb class="is-6"></article-thumb>
             </div>
           </div>
         </div>
@@ -287,3 +94,18 @@
 
   </div>
 </template>
+<script>
+  import ProductThumb from '@/Shop/ProductThumb';
+  import GComment from '@/Community/Comment';
+  import NewComment from '@/Community/NewComment';
+  import ArticleThumb from '@/Community/ArticleThumb';
+
+  export default {
+    components: {
+      ProductThumb,
+      GComment,
+      NewComment,
+      ArticleThumb,
+    },
+  };
+</script>
