9468f08c8851a4ea504c5ac4d094696fdc1e4d1c
seifsayed <seifsayed@gmail.com>
2017-06-26 16:34:52 +0200

    Add Community Tabs [ wall - articles ]

---
+++ src/Pages/Community/Articles.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="columns is-multiline">
+    <article-thumb class="is-12"></article-thumb>
+    <article-thumb class="is-12"></article-thumb>
+    <article-thumb class="is-12"></article-thumb>
+    <article-thumb class="is-12"></article-thumb>
+  </div>
+</template>
+<script>
+  import ArticleThumb from '@/Community/ArticleThumb';
+
+  export default {
+    components: {
+      ArticleThumb,
+    },
+  };
+</script>
+
---
+++ src/Pages/Community/Wall.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="columns">
+      <new-comment></new-comment>
+    </div>
+    <div class="columns is-multiline">
+      <g-comment class="is-12"></g-comment>
+      <g-comment class="is-12"></g-comment>
+      <g-comment class="is-12"></g-comment>
+    </div>
+  </div>
+</template>
+<script>
+  import GComment from '@/Community/Comment';
+  import NewComment from '@/Community/NewComment';
+
+  export default {
+    components: {
+      GComment,
+      NewComment,
+    },
+  };
+</script>
